66fb2ab2701f4a73edef19c72fc83d28049720e7
Br0ncosfan58 <mwasserstein2025@kehillahstudent.org>
2024-09-17 23:28:59 -0700

    a02 daily exercise

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 import turtle
+from turtle import (forward, backward, left, right, exitonclick, speed, penup, pendown)
+speed(5)
+
+right(180)
+forward(50)
+
+for i in range(3):
+    forward(200)
+    left(90)
+
+for i in range(2):
+    forward(150)
+    left(90)
+
+for i in range(2):
+    forward(100)
+    left(90)
+
+for i in range(2):
+    forward(50)
+    left(90)
+
+for i in range(2):
+    forward(20)
+    left(90)
+
+for i in range(1):
+    forward(10)
+    left(90)
+turtle.exitonclick
